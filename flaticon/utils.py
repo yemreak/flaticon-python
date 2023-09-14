8340ef31c67dda77b6843f17929225a10adb4819
Yunus Emre <49655146+yemreak@users.noreply.github.com>
2023-09-14 21:01:28 +0300

    add compatibility

--- flaticon/utils.py
+++ flaticon/utils.py
@@ -4,7 +4,7 @@ from aiofiles import open as aioopen
 from aiohttp import ClientSession
 
 
-async def download(url: str, filepath: str | Path, session: ClientSession) -> Path:
+async def download(url: str, filepath: Path, session: ClientSession) -> Path:
     async with session.get(url) as response:
         async with aioopen(filepath, "wb") as file:
             async for chunk in response.content.iter_chunked(4096):
